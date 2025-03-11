e106c11ad2e4e0df6abc9446727c8736f4169b27
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-11 11:07:47 +0800

    sys_newuname: only spoof for root

--- kernel/sys.c
+++ kernel/sys.c
@@ -1192,6 +1192,15 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		goto bypass_orig_flow;
 #endif
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (current_uid().val == 0 && 
+		(!strncmp(current->comm, "bpfloader", 9) ||
+		!strncmp(current->comm, "netbpfload", 10) ||
+	    	!strncmp(current->comm, "netd", 4))) {
+		strcpy(tmp.release, "5.4.186");
+		pr_info("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
+// make sure bpf uname spoof is prioritized
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 bypass_orig_flow:
 #endif
